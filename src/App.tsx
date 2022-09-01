95f2e950b66507a48f703cb590b8ce49b434db3c
George <73098841+NoNameGeorge@users.noreply.github.com>
2022-09-01 14:10:55 +0300

    Add libriares and base files and folders

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Routes } from 'react-router-dom';
+
+const App: FC = () => {
+    return (
+        <Routes>
+            
+		</Routes>
+    );
+};
+
+export default App;
